ef206812da564ca1bc5a56ee03d765cd429e7d4d
adminmoi <pcanhtuandat@gmail.com>
2025-08-03 11:00:52 +0700

    - Học đến video số 186. Spring Boot - Spring MVC Binding Request Params - Coding - Tóm tắt: Thay vì ở /processFormVersionTwo ta tạo method với 2 tham số là HttpServletRequest request, Model model và trong method ta muốn đọc data từ form thì phải String theName = request.getParameter("studentName"); thì ở /processFormVersionThree ta chỉ cần dùng annotation @RequestParam("studentName") String theName và Spring sẽ tự động đọc data giúp ta, không cần dòng String theName = request.getParameter("studentName");

--- 06-thymeleaf/01-thymeleafdemo-helloworld/thymeleafdemo/src/main/java/spring/learning/thymeleafdemo/controller/HelloWorldController.java
+++ 06-thymeleaf/01-thymeleafdemo-helloworld/thymeleafdemo/src/main/java/spring/learning/thymeleafdemo/controller/HelloWorldController.java
@@ -5,6 +5,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
@@ -39,5 +40,23 @@ public class HelloWorldController {
 
 
         return "helloworld"; // helloworld là tên của file helloworld.html trong thư mục templates
+    }
+
+    // controler to read form data and add data to the model version three
+    @RequestMapping("/processFormVersionThree")
+    public String letsShoutDude2(@RequestParam("studentName") String theName, Model model){
+
+        // no need to read the request parameter from the HTML form, Spring will do it for us as we use @RequestParam
+
+        // convert the data to all caps
+        theName = theName.toUpperCase();
+
+        // create the message
+        String result = "Hey my friend from v3! " + theName;
+
+        // add message to the model
+        model.addAttribute("message", result);
+
+        return "helloworld";
     }
 }
