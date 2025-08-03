99eba3c1639e3f7d8f773673d6c92d50795f28f2
adminmoi <pcanhtuandat@gmail.com>
2025-08-03 11:12:33 +0700

    Update lại cấu trúc project, tạo 1 project practice cho Spring MVC ở folder practice

---
+++ 00-practice/02-practiceSpringMVC/src/main/java/com/example/practiceSpringMVC/PracticeSpringMvcApplication.java
@@ -0,0 +1,13 @@
+package com.example.practiceSpringMVC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeSpringMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeSpringMvcApplication.class, args);
+	}
+
+}
---
+++ 00-practice/02-practiceSpringMVC/src/test/java/com/example/practiceSpringMVC/PracticeSpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practiceSpringMVC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeSpringMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
