993a144f211bf1c55f24ab5483709e73828e5daf
adminmoi <pcanhtuandat@gmail.com>
2025-08-03 10:50:27 +0700

    - Học đến video số 184. Spring Boot - Adding Data to Spring MVC Model - Coding - Part 2 - Tóm tắt: Thêm data vào model

--- 06-thymeleaf/01-thymeleafdemo-helloworld/thymeleafdemo/src/main/java/spring/learning/thymeleafdemo/controller/HelloWorldController.java
+++ 06-thymeleaf/01-thymeleafdemo-helloworld/thymeleafdemo/src/main/java/spring/learning/thymeleafdemo/controller/HelloWorldController.java
@@ -1,6 +1,9 @@
 package spring.learning.thymeleafdemo.controller;
 
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -15,6 +18,26 @@ public class HelloWorldController {
     // controller method to process input form
     @RequestMapping("/processForm")
     public String processForm(){
+        return "helloworld"; // helloworld là tên của file helloworld.html trong thư mục templates
+    }
+
+    // controler to read form data and add data to the model
+    @RequestMapping("/processFormVersionTwo")
+    public String letsShoutDude(HttpServletRequest request, Model model){
+
+        // read the request parameter from the HTML form
+        String theName = request.getParameter("studentName");
+
+        // convert the data to all caps
+        theName = theName.toUpperCase();
+
+        // create the message
+        String result = "Yo! " + theName;
+
+        // add message to the model
+        model.addAttribute("message", result);
+
+
         return "helloworld"; // helloworld là tên của file helloworld.html trong thư mục templates
     }
 }
