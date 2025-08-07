95f54910b8eb1571cdd88bc6437c72db45d3f498
adminmoi <pcanhtuandat@gmail.com>
2025-08-07 11:36:50 +0700

    - Học đến video số 214. Spring Boot - Spring MVC Validation - Validate a Number Range - Coding - Tóm tắt: + Annotation mới: @Min(value=.., message=""), @Max(value=..., message="") để quy định giá trị Min, Max của data input vào

--- 06-thymeleaf/02-validationdemo/src/main/java/com/example/validationdemo/model/Customer.java
+++ 06-thymeleaf/02-validationdemo/src/main/java/com/example/validationdemo/model/Customer.java
@@ -1,22 +1,26 @@
 package com.example.validationdemo.model;
 
+import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public class Customer {
     private String firstName;
-
     @NotNull(message = "Last name is required")
     @Size(min=1, message = "Last name must be at least 1 character long")
     private String lastName;
+    @Min(value=0, message = "Age must be >0")   // Minimum
+    @Max(value=200, message = "Age must be < 200")
+    private int age;
 
     // Constructors
     public Customer() {
     }
-    public Customer(String firstName, String lastName) {
+    public Customer(String firstName, String lastName, int age) {
         this.firstName = firstName;
         this.lastName = lastName;
+        this.age = age;
     }
 
     // Getters and Setters
@@ -31,6 +35,12 @@ public class Customer {
     }
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
     }
 
 }
