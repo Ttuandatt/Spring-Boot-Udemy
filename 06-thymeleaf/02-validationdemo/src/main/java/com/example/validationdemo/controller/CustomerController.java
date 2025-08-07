ef6a1ca9368c813727db1d3dfb3e37a170130272
adminmoi <pcanhtuandat@gmail.com>
2025-08-07 10:51:30 +0700

    - Học đến video số 212. Spring Boot - Spring MVC Validation - @InitBinder - Coding - Tóm tắt: + Xử lý trường hợp data input vào toàn là whitespace, thì ta trim nó thành null, còn nếu data input vào có chứa whitespace thì trim whitespace đi + Annotation mới: @InitBinder + Tham số mới: WebDataBinder, StringTrimmerEditor

--- 06-thymeleaf/02-validationdemo/src/main/java/com/example/validationdemo/controller/CustomerController.java
+++ 06-thymeleaf/02-validationdemo/src/main/java/com/example/validationdemo/controller/CustomerController.java
@@ -2,15 +2,26 @@ package com.example.validationdemo.controller;
 
 import com.example.validationdemo.model.Customer;
 import jakarta.validation.Valid;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CustomerController {
+
+    // add an initbinder ... to remove leading and trailing whitespace
+    @InitBinder
+    public void initBinder(WebDataBinder binder) {
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true); // true means trim empty strings to null
+        binder.registerCustomEditor(String.class, stringTrimmerEditor); // Register the StringTrimmerEditor for all String fields. This means that any String field in the Customer class will be trimmed of leading and trailing whitespace.
+    }
+
     @GetMapping("/showCustomerForm")
     public String showCustomerForm(Model model) {
         // This method will return the name of the view to be rendered
@@ -25,6 +36,7 @@ public class CustomerController {
         // @Valid annotation is used to trigger validation on the Customer object
         // BindingResult is used to hold the result of the validation and any errors
 
+        System.out.println("Last name: |" + customer.getLastName()+"|");
 
         // Check for validation errors
         if(bindingResult.hasErrors()) {
@@ -32,5 +44,6 @@ public class CustomerController {
         }else{
             return "customer-confirmation"; // Assuming you have a Thymeleaf template named customer-confirmation.html
         }
+
     }
 }
