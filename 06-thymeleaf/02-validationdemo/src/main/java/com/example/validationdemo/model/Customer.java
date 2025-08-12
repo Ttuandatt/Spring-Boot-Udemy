61cf11b21bd9fa49498ac295425cb648865439ad
adminmoi <pcanhtuandat@gmail.com>
2025-08-12 21:05:54 +0700

    - Học đến video số 217. Spring Boot - Spring MVC Validation - Make Integer Fields Required - Tóm tắt: + Nói chung là thêm cái validation là không được để trống input kiểu số nguyên. Chưa có gì mới.

--- 06-thymeleaf/02-validationdemo/src/main/java/com/example/validationdemo/model/Customer.java
+++ 06-thymeleaf/02-validationdemo/src/main/java/com/example/validationdemo/model/Customer.java
@@ -7,9 +7,10 @@ public class Customer {
     @NotNull(message = "Last name is required")
     @Size(min=1, message = "Last name must be at least 1 character long")
     private String lastName;
+    @NotNull(message = "Age is required") // @NotNull: This annotation ensures that the age field cannot be null.
     @Min(value=0, message = "Age must be >0")   // Minimum
     @Max(value=200, message = "Age must be < 200")
-    private int age;
+    private Integer age;
 
     @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 alphanumeric characters are allowed") // regexp = "^[a-zA-Z0-9]{5}": // This regex pattern ensures that the postal code consists of exactly 5 alphanumeric characters (letters and digits).
     private String postalCode;
@@ -17,7 +18,7 @@ public class Customer {
     // Constructors
     public Customer() {
     }
-    public Customer(String firstName, String lastName, int age, String postalCode) {
+    public Customer(String firstName, String lastName, Integer age, String postalCode) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.age = age;
@@ -37,10 +38,10 @@ public class Customer {
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
-    public int getAge() {
+    public Integer getAge() {
         return age;
     }
-    public void setAge(int age) {
+    public void setAge(Integer age) {
         this.age = age;
     }
     public String getPostalCode() {
